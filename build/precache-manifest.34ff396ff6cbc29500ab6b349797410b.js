20795ba752a3e9af655b1e9b96faf0c062ca0e38
Gothama Nayaneetha Rajawasam <49790149+Gothama@users.noreply.github.com>
2020-10-02 19:09:53 +0530

    deploy

---
+++ build/precache-manifest.34ff396ff6cbc29500ab6b349797410b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6f64d46fdbd80a489f0c304be70121",
+    "url": "/NutrazyWeb/index.html"
+  },
+  {
+    "revision": "9010fe215211fc1077e2",
+    "url": "/NutrazyWeb/static/css/main.9e70dc45.chunk.css"
+  },
+  {
+    "revision": "c494ce2f1a8720440780",
+    "url": "/NutrazyWeb/static/js/2.0ce70119.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/NutrazyWeb/static/js/2.0ce70119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9010fe215211fc1077e2",
+    "url": "/NutrazyWeb/static/js/main.50cf5d7f.chunk.js"
+  },
+  {
+    "revision": "6574e1112ed342b618ee",
+    "url": "/NutrazyWeb/static/js/runtime-main.3831be8d.js"
+  },
+  {
+    "revision": "06ab33ae3e2cbae0f509cd05f2ee6793",
+    "url": "/NutrazyWeb/static/media/boxicons.06ab33ae.eot"
+  },
+  {
+    "revision": "5bbebd73a37de0cc242e872b4483269e",
+    "url": "/NutrazyWeb/static/media/boxicons.5bbebd73.woff"
+  },
+  {
+    "revision": "66d9fae4a22d74c2618f44c585a0c1a8",
+    "url": "/NutrazyWeb/static/media/boxicons.66d9fae4.svg"
+  },
+  {
+    "revision": "72589e162de78952d4d6b5ce8b832175",
+    "url": "/NutrazyWeb/static/media/boxicons.72589e16.ttf"
+  },
+  {
+    "revision": "aeaf4ee6e2c712532f982c8a4dd68b16",
+    "url": "/NutrazyWeb/static/media/boxicons.aeaf4ee6.woff2"
+  },
+  {
+    "revision": "31cb05f3e1c4697812b2104049d21c55",
+    "url": "/NutrazyWeb/static/media/contact-bg.31cb05f3.jpg"
+  },
+  {
+    "revision": "f042c2ac7fe16211f6c2806e04fdebad",
+    "url": "/NutrazyWeb/static/media/cta-bg.f042c2ac.jpg"
+  },
+  {
+    "revision": "139e74e298bca37a25d2bd5868552e04",
+    "url": "/NutrazyWeb/static/media/fontawesome-webfont.139e74e2.svg"
+  },
+  {
+    "revision": "706450d7bba6374ca02fe167d86685cb",
+    "url": "/NutrazyWeb/static/media/fontawesome-webfont.706450d7.ttf"
+  },
+  {
+    "revision": "97493d3f11c0a3bd5cbd959f5d19b699",
+    "url": "/NutrazyWeb/static/media/fontawesome-webfont.97493d3f.woff2"
+  },
+  {
+    "revision": "d9ee23d59d0e0e727b51368b458a0bff",
+    "url": "/NutrazyWeb/static/media/fontawesome-webfont.d9ee23d5.woff"
+  },
+  {
+    "revision": "f7c2b4b747b1a225eb8dee034134a1b0",
+    "url": "/NutrazyWeb/static/media/fontawesome-webfont.f7c2b4b7.eot"
+  },
+  {
+    "revision": "acbad608bcf83d3adaccaae949e22997",
+    "url": "/NutrazyWeb/static/media/food.acbad608.jpeg"
+  },
+  {
+    "revision": "b9f6fc52127aec71982932c70a96ad85",
+    "url": "/NutrazyWeb/static/media/foodBlogportrait.b9f6fc52.jpg"
+  },
+  {
+    "revision": "ae2508775e7b40b7f9678a27610e3578",
+    "url": "/NutrazyWeb/static/media/gym-video.ae250877.mp4"
+  },
+  {
+    "revision": "625aee00f6836ff881a3aad444f47062",
+    "url": "/NutrazyWeb/static/media/nutrazyBlog.625aee00.jpg"
+  },
+  {
+    "revision": "4a1c15dbf1d89855c1ef8e81fce8f921",
+    "url": "/NutrazyWeb/static/media/p1.4a1c15db.png"
+  },
+  {
+    "revision": "02e364823438fedfc552123239968bf6",
+    "url": "/NutrazyWeb/static/media/schedule-bg.02e36482.jpg"
+  },
+  {
+    "revision": "0a63076277a6405d22883465bbcfad43",
+    "url": "/NutrazyWeb/static/media/slide-01.0a630762.jpg"
+  },
+  {
+    "revision": "6199a2ce6421905f001e3046c02bf336",
+    "url": "/NutrazyWeb/static/media/trainer-1.6199a2ce.jpg"
+  },
+  {
+    "revision": "5a6c841c01f957d21afe705bf1754ce0",
+    "url": "/NutrazyWeb/static/media/training-image-01.5a6c841c.jpg"
+  }
+]);
